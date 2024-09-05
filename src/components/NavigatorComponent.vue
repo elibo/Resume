e67e59fbe1350dc297b28befdfca6eb0ffc0511b
Elibo <elibo@live.com>
2024-09-05 12:24:27 +0200

    fixes mobile

--- src/components/NavigatorComponent.vue
+++ src/components/NavigatorComponent.vue
@@ -4,36 +4,20 @@
       <div class="row">
         <div class="col-lg-12">
           <nav class="navbar navbar-expand-lg">
-            <button
-              class="navbar-toggler"
-              type="button"
-              data-toggle="collapse"
-              data-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span class="toggler-icon"></span>
-              <span class="toggler-icon"></span>
-              <span class="toggler-icon"></span>
-            </button>
-
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav ml-auto">
-                <li class="nav-item active">
-                  <a class="page-scroll" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                  <a class="page-scroll" href="#about">About</a>
-                </li>
-                <li class="nav-item">
-                  <a class="page-scroll" href="#hobbies">Hobbies</a>
-                </li>
-                <li class="nav-item">
-                  <a class="page-scroll" href="#work">Work</a>
-                </li>
-              </ul>
-            </div>
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item active">
+                <a class="page-scroll" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="page-scroll" href="#about">About</a>
+              </li>
+              <li class="nav-item">
+                <a class="page-scroll" href="#hobbies">Hobbies</a>
+              </li>
+              <li class="nav-item">
+                <a class="page-scroll" href="#work">Work</a>
+              </li>
+            </ul>
           </nav>
         </div>
       </div>
--- src/components/WorkComponent.vue
+++ src/components/WorkComponent.vue
@@ -1,3 +1,7 @@
+<script lang="ts">
+const showOverlay = false
+</script>
+
 <template>
   <section id="work" class="work-area pt-125 pb-130">
     <div class="container">
@@ -9,54 +13,45 @@
         </div>
       </div>
       <div class="row">
-        <div class="col-lg-4 col-md-6 col-sm-6">
+        <div class="col-lg-4 col-md-6 col-sm-8">
           <div class="single-work text-center mt-30">
             <div class="work-image">
-              <p>
+              <p class="work-title">
                 Citecam <br />
-                2016-2019
+                <span> 2016-2019 </span>
               </p>
-            </div>
-            <div class="work-overlay">
-              <div class="work-content">
-                <p class="work-title">
-                  My first job ever as a developer. I learned Angular, Typescript, React, node,
-                  MongoDB and a long list of technologies
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-4 col-md-6 col-sm-6">
-          <div class="single-work text-center mt-30">
-            <div class="work-image">
               <p>
-                Plexus <br />
-                2019-2022
+                My first job ever as a developer. I learned Angular, Typescript, React, node,
+                MongoDB and a long list of technologies
               </p>
-            </div>
-            <div class="work-overlay">
-              <div class="work-content">
-                <p class="work-title">
-                  Had to create from scracht a custom component library with Storybook and Styled
-                  Components
-                </p>
-              </div>
             </div>
           </div>
         </div>
-        <div class="col-lg-4 col-md-6 col-sm-6">
+        <div class="col-lg-4 col-md-6 col-sm-8">
           <div class="single-work text-center mt-30">
             <div class="work-image">
-              <p>Sparta <br />2022-2024</p>
+              <p class="work-title">
+                Plexus <br />
+                <span> 2019-2022 </span>
+              </p>
+              <p>
+                Had to create from scracht a custom component library with Storybook and Styled
+                Components
+              </p>
             </div>
-            <div class="work-overlay">
-              <div class="work-content">
-                <p class="work-title">
-                  Main tools were ReactJS, ChartJS, CircleCI and a custom component library. We had
-                  to migrate from old vanilla JS to a proper TS version
-                </p>
-              </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-8">
+          <div class="single-work text-center mt-30">
+            <div class="work-image">
+              <p class="work-title">
+                Sparta <br />
+                <span> 2022-2024 </span>
+              </p>
+              <p>
+                Main tools were ReactJS, ChartJS, CircleCI and a custom component library. We had to
+                migrate from old vanilla JS to a proper TS version
+              </p>
             </div>
           </div>
         </div>
